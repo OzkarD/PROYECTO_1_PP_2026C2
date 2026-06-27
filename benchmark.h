3900a76113713525707f4b036ee41408be8422d5
Joaquin Casarin <11091@soyunaq.mx>
2026-06-27 17:17:17 -0600

    Cambio no. 2 del benchmark.h

--- benchmark.h
+++ benchmark.h
@@ -3,6 +3,6 @@
 
 typedef void (*Sorting_Method)(const int* v, int size);
 
-ResultTime benchmarkSortingMethod(Sequencial_Method method, const int* v, int size, int repetitions);
+ResultTime benchmarkSortingMethod(Sorting_Method method, const int* v, int size, int repetitions);
 
 void printBenchmarkTable(const ResultTime* seqResult, const ResultTime* parResult);
