b3a13f9a153ec3174a2a257f86bdd48bda8fdef7
Jhonathan Lorenzo Rivas Guerrero <bigwarrior115@gmail.com>
2026-06-27 15:03:16 -0600

    Added: getUserData
    Se compila el codigo por primera vez, probando las funciones de vector tools y getUserData, build correcto

--- PROYECTO_1_PP_2026C2.cpp
+++ PROYECTO_1_PP_2026C2.cpp
@@ -3,10 +3,9 @@
 
 #include <iostream>
 #include <time.h>
-#include <omp.h>
-#include <chrono>
 
 #include "config.h"
+#include "vector_tools.h"
 
 #include "bitonic_sort.h"
 #include "bubble_sort.h"
@@ -18,8 +17,31 @@
 
 int main() 
 {
-	UserData data;
+	srand((unsigned)time(NULL));
+	UserData data = getUserData();
+
+	const int N = 1 << data.bit_shift;
+	int* vector = createVector(N);
+	fillRandom(vector, N, data.min_value, data.max_value);
+	printSample(vector, N, 10);
 	
+	int* vector_copy = createVector(N);// El vector original no se toca, para que cada algoritmo pueda ordenar y no modifique el vector original
+
+	//copyVector(vector, vector_copy, N);
+
+	/*
+	Implementacion de las funciones con benchmark
+	*/
+
+	/*
+	Impresion del benchmark
+	*/
+
+	/*
+	Impresion de metrics
+	*/
+
+	destroyVector(vector);
 	return 0;
 }
 
