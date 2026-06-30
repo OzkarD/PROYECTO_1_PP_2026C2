ed37955c3c71e33d606c56f3122ce801a7f2cc78
yaxl_chp <ychalepech@gmail.com>
2026-06-29 18:46:04 -0600

    Sequential selection sort done

--- selection_sort.cpp
+++ selection_sort.cpp
@@ -3,14 +3,33 @@
 
 using namespace std;
 
-void sequential::selection_sort(int* v, int n) 
+void sequential::selection_sort(int* v, int n)
 {
-    cout << "Ejecutando selection_sort secuencial..." << endl;
-    return;
+    cout << "Ejecutando selection sort en secuencial...\n" << endl;
+
+    for (int i = 0; i < n-1; i++)
+    {
+        int value_min = i;
+
+        for (int j = i + 1; j < n; j++)
+        {
+            if (v[j] < v[value_min])
+                value_min = j;
+        }
+
+        swap(v[i], v[value_min]);
+    }
 }
 
 void parallel::selection_sort(int* v, int n) 
 {
     cout << "Ejecutando selection_sort paralelo..." << endl;
     return;
+}
+
+void swap(int& a, int& b)
+{
+	int temp = a;
+	a = b;
+	b = temp;
 }
