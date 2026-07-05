3fab4dd1c4d724f86afdb4867f652da6c02ddd0e
Joaquin Casarin <11091@soyunaq.mx>
2026-07-05 11:24:28 -0600

    Implementacion de insertion sort y su testfile

---
+++ Test_file_pareja3.cpp
@@ -0,0 +1,44 @@
+// PROYECTO_1_PP_2026C2.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+#include <time.h>
+
+#include "config.h"
+#include "vector_tools.h"
+#include "benchmark.h"
+#include "metrics.h"
+
+#include "bitonic_sort.h"
+#include "bubble_sort.h"
+#include "insertion_sort.h"
+#include "merge_sort.h"
+#include "quick_sort.h"
+#include "selection_sort.h"
+
+//Implementado por el Integrador
+#include "vector_tools.cpp"
+#include "insertion_sort.cpp"
+
+int main()
+{
+	srand((unsigned)time(NULL));
+	//UserData data = getUserData();
+
+	const int N = 50000;
+	int* vector = createVector(N);
+	fillRandom(vector, N,-50, 50);
+	printSample(vector, N, 20);
+
+	sequential::insertion_sort(vector, N);
+
+	for (int i = 0; i < 20; i++)
+	{
+		printf("%d ", vector[i]);
+	}
+
+	destroyVector(vector);
+
+	return 0;
+}
+
--- insertion_sort.cpp
+++ insertion_sort.cpp
@@ -6,6 +6,20 @@ using namespace std;
 void sequential::insertion_sort(int* v, int n) 
 {
     cout << "Ejecutando insertion_sort secuencial..." << endl;
+    for (int i = 1; i < n; i++)
+    {
+        int key = v[i];
+        int j = i - 1;
+
+		while (j >= 0 && v[j] > key)
+		{
+			v[j + 1] = v[j];
+			j--;
+		}
+
+        v[j + 1] = key;
+        
+    }
     return;
 }
 
