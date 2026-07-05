2a9e0689d23f65581f41c22393c4f8e317667536
Moronicfoil <ibarrapaz0405@gmail.com>
2026-07-04 19:20:17 -0600

    sequential insertion sort done

---
+++ Test_file_pareja3.cpp
@@ -0,0 +1,41 @@
+// PROYECTO_1_PP_2026C2.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+#include <time.h>
+
+#include "config.h"
+#include "vector_tools.h"
+#include "benchmark.h"
+#include "metrics.h"
+
+#include "bitonic_sort.h"
+#include "bubble_sort.h"
+#include "insertion_sort.h"
+#include "merge_sort.h"
+#include "quick_sort.h"
+#include "selection_sort.h"
+
+
+int main()
+{
+	srand((unsigned)time(NULL));
+	//UserData data = getUserData();
+
+	const int N = 20;
+	int* vector = createVector(N);
+	fillRandom(vector, N,0, 50);
+	printSample(vector, N, 20);
+
+	sequential::insertion_sort(vector, N);
+
+	for (int i = 0; i < N; i++)
+	{
+		printf("%d ", vector[i]);
+	}
+
+	destroyVector(vector);
+
+	return 0;
+}
+
