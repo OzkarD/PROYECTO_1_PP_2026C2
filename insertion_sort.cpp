793c4699002bcd43cc785ee02ea9400a96b81838
Jhonathan Lorenzo Rivas Guerrero <bigwarrior115@gmail.com>
2026-07-11 21:12:55 -0600

    Parallel Insertion Sort Integrated

--- insertion_sort.cpp
+++ insertion_sort.cpp
@@ -1,5 +1,7 @@
 #include "insertion_sort.h"
 #include <iostream>
+#include <omp.h>
+#include "vector_tools.h"
 
 using namespace std;
 
@@ -9,8 +11,40 @@ void sequential::insertion_sort(int* v, int n)
     return;
 }
 
-void parallel::insertion_sort(int* v, int n) 
+void parallel::insertion_sort(int* v, int n)
 {
     cout << "Ejecutando insertion_sort paralelo..." << endl;
-    return;
+    if (v == nullptr || n <= 1)
+        return;
+
+    for (int phase = 0; phase < n; phase++)
+    {
+        if (phase % 2 == 0)
+        {
+#pragma omp parallel for
+            for (int i = 1; i < n; i += 2)
+            {
+                if (v[i - 1] > v[i])
+                {
+                    int temp = v[i];
+                    v[i] = v[i - 1];
+                    v[i - 1] = temp;
+                }
+            }
+        }
+        else
+        {
+#pragma omp parallel for
+            for (int i = 1; i < n - 1; i += 2)
+            {
+                if (v[i] > v[i + 1])
+                {
+                    int temp = v[i];
+                    v[i] = v[i + 1];
+                    v[i + 1] = temp;
+                }
+            }
+        }
+    }
+	checkSorted(v, n);
 }
