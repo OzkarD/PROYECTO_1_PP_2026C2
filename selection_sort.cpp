68250cb6fb21db165e4f9eb27afd7738f14cdcd7
Jhonathan Lorenzo Rivas Guerrero <bigwarrior115@gmail.com>
2026-07-12 23:46:14 -0600

    Pararllel selection sort Integrated

--- selection_sort.cpp
+++ selection_sort.cpp
@@ -1,6 +1,8 @@
 #include "selection_sort.h"
 #include "vector_tools.h"
 #include <iostream>
+#include <omp.h>
+#include <vector>
 
 using namespace std;
 
@@ -8,7 +10,7 @@ void sequential::selection_sort(int* v, int n)
 {
     cout << "Ejecutando selection sort en secuencial..." << endl;
 
-    for (int i = 0; i < n-1; i++)
+    for (int i = 0; i < n - 1; i++)
     {
         int value_min = i;
 
@@ -19,18 +21,50 @@ void sequential::selection_sort(int* v, int n)
         }
         swap(v[i], v[value_min]);
     }
-	checkSorted(v, n);
+    checkSorted(v, n);
 }
 
-void parallel::selection_sort(int* v, int n) 
+void parallel::selection_sort(int* v, int n)
 {
     cout << "Ejecutando selection_sort paralelo..." << endl;
-    return;
+
+    int num_threads = omp_get_max_threads(); // número de hilos disponibles
+    vector<int> mins(num_threads);           // arreglo dinámico, uno por hilo
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        int value_min = i;
+
+#pragma omp parallel
+        {
+            int tid = omp_get_thread_num();
+            int local_min = value_min;
+
+#pragma omp for
+            for (int j = i + 1; j < n; j++)
+            {
+                if (v[j] < v[local_min])
+                    local_min = j;
+            }
+
+            mins[tid] = local_min;
+        }
+
+        for (int t = 0; t < num_threads; t++)
+        {
+            if (v[mins[t]] < v[value_min])
+                value_min = mins[t];
+        }
+
+        swap(v[i], v[value_min]);
+    }
+
+    checkSorted(v, n);
 }
 
-void swap(int& a, int& b)
+static void swap(int& a, int& b)
 {
-	int temp = a;
-	a = b;
-	b = temp;
+    int temp = a;
+    a = b;
+    b = temp;
 }
